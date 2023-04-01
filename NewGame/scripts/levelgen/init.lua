1bbd7308bd954a9d2dba36325eab17ff1f9ed724
Aril <123@123.com>
2023-04-01 13:28:16 +0200

    add perlin noise generation

---
+++ NewGame/scripts/levelgen/init.lua
@@ -0,0 +1,27 @@
+prl = {}
+function init_level()
+
+    prl = perlin()
+    prl:init()
+    prl:getmapdata(0,150,0,150)
+end
+
+function draw_level()
+    local st = "values="
+    draw_rect(0,0)
+    draw_rect(1,1)
+
+    for x = 1,26,1 do 
+        for y = 1,26,1 do 
+            --st ..= prl.fmap[x][y]
+            draw_rect(x,y, flr(prl.fmap[x][y]))
+        end
+    end
+    
+    --printh(st, "@clip")
+end
+
+function draw_rect(x,y, color)
+    size = 4
+    rectfill(x*size, y*size, (x*size)+size-1, (y*size)+size-1, color)
+end
