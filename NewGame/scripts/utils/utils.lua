3e7dadd310b3242d65ad4491f7e75a76df716e8d
FrederikHR <frederik.rosenvinge@gmail.com>
2023-04-01 20:31:38 +0200

    moved flash to util

--- NewGame/scripts/utils/utils.lua
+++ NewGame/scripts/utils/utils.lua
@@ -104,6 +104,18 @@ function fade_in() -- reversed for-loops. not sure if working correctly
 		flip()
 	end
 end
+
+function flash()
+    flashframe+=1
+    if flashframe>flash_speed then
+        flashframe=0
+        if (flash_color==8) then 
+            flash_color=9
+        else
+            flash_color=8
+        end
+    end
+end
 --[[
 function damage_enemy(enemy,item,hand)
 	-- Base damage lookup
