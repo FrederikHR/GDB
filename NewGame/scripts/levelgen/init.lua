9a7634dd09e6981ef7cb975272d44210f35029cf
Tor-Arne <tornord94@gmail.com>
2023-04-08 21:39:13 +0200

    added textured floors and only drawing within certain distanc

--- NewGame/scripts/levelgen/init.lua
+++ NewGame/scripts/levelgen/init.lua
@@ -9,29 +9,39 @@ function init_levelgen()
 end
 
 function draw_level()
+    local scale=maze[1][1].scale
+    local ox=maze[1][1].ox
+    local oy=maze[1][1].oy
     for x = 1,#maze do 
-        for y = 1,#maze[1] do 
-            draw_maze_room(x,y,maze[x][y],maze[1][1].scale,maze[1][1].ox,maze[1][1].oy)
+        for y = 1,#maze[1] do
+            if (p.curr_mt== nil or abs(p.curr_mt.x-x)<3 and abs(p.curr_mt.y-y)<3) draw_maze_room(x,y,maze[x][y],scale,ox,oy)
         end
     end
 end
 
 function draw_maze_room(x,y,room,scale,ox,oy)
-    local size = scale
 
-    rectfill(x*size+ox, y*size+oy, x*size+size+ox, y*size+size+oy, 7)
+    --rectfill(x*scale+ox, y*scale+oy, x*scale+scale+ox, y*scale+scale+oy, 7)
+    draw_tile("floor",x,y,scale,ox,oy)
     if room.goal then
-        rectfill(x*size+ox, y*size+oy, x*size+size+ox, y*size+size+oy, 11)
+        rectfill(x*scale+ox, y*scale+oy, x*scale+scale+ox, y*scale+scale+oy, 11)
     end
     if room.is_wall then
-        rectfill(x*size+ox, y*size+oy, x*size+size+ox, y*size+size+oy, 1)
+        rectfill(x*scale+ox, y*scale+oy, x*scale+scale+ox, y*scale+scale+oy, 1)
         if room.goal then
-            rectfill(x*size+ox, y*size+oy, x*size+size+ox, y*size+size+oy, 9)
+            rectfill(x*scale+ox, y*scale+oy, x*scale+scale+ox, y*scale+scale+oy, 9)
         end
     end
 
 end
 
-function draw_tile(x,y,scale,ox,oy)
-    print("TODO")
+function draw_tile(type,x,y,scale,ox,oy)
+    local walls={}
+    local floors={112,113,114}
+    srand(x+y+scale)
+    for i=0,scale\8 do
+        for j=0,scale\8 do
+            if (type=="floor") spr(rnd(floors),x*scale+ox+i*8,y*scale+oy+j*8)
+        end
+    end
 end
