4a3ad1d93ac6ced6df3574fc385169e7ed5338a4
Tor-Arne <tornord94@gmail.com>
2023-04-14 22:57:00 +0200

    fade_out at next floor

--- NewGame/scripts/entities/player.lua
+++ NewGame/scripts/entities/player.lua
@@ -134,8 +134,10 @@ function move_player()
     player_collide_map(p,"down")
     
 
-    if (p.curr_mt.x==game_state.goal[1] and p.curr_mt.y==game_state.goal[2]) init_level(game_state.level)
-
+    if (p.curr_mt.x==game_state.goal[1] and p.curr_mt.y==game_state.goal[2]) then
+        fade_out()
+        init_level(game_state.level)
+    end
     --update position
     p.x+=p.dx
     p.y+=p.dy
